364540aeae491cfe0b5c177bccebcfe8ae65aaf1
godrei <krisztian.godrei@bitrise.io>
2022-01-18 17:24:25 +0100

    Add initial project

---
+++ ProfileTest/ProfileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileTestApp.swift
+//  ProfileTest
+//
+//  Created by Krisztian Godrei on 2022. 01. 18..
+//
+
+import SwiftUI
+
+@main
+struct ProfileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
